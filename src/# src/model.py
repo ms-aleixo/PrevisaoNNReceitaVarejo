a18e694c77588eb61c7c22dca62b61d3b8257014
ms-aleixo <165584972+ms-aleixo@users.noreply.github.com>
2024-03-31 20:21:53 +0200

    Create model.py

---
+++ src/# src/model.py
@@ -0,0 +1,15 @@
+# src/model.py
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.callbacks import EarlyStopping, ReduceLROnPlateau
+import os
+
+def build_model(input_shape, hyperparameters):
+    model = Sequential([
+        Dense(units=hyperparameters['units'], activation=hyperparameters['activation'], input_shape=(input_shape,)),
+        Dense(units=1, activation='linear')
+    ])
+    model.compile(optimizer=Adam(learning_rate=hyperparameters['learning_rate']), loss='mean_squared_error', metrics=['mean_absolute_error'])
+    return model
